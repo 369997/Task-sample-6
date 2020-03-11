e6165e5690343cb2bb3baee0f3d665e47f98b735
369997 <369997@gmail.com>
2020-03-11 10:06:58 +0300

    finish task sample 6

---
+++ main.js
@@ -0,0 +1,35 @@
+$('#toggle1').click(function() {
+  $('.picture__weather1').toggle();
+  $('.text__description1').toggle();
+  $('.title__h31').toggle();
+});
+$('#toggle2').click(function() {
+  $('.picture__weather2').toggle();
+  $('.text__description2').toggle();
+  $('.title__h32').toggle();
+});
+$('#toggle3').click(function() {
+  $('.picture__weather3').toggle();
+  $('.text__description3').toggle();
+  $('.title__h33').toggle();
+});
+$('#toggle4').click(function() {
+  $('.picture__weather4').toggle();
+  $('.text__description4').toggle();
+  $('.title__h34').toggle();
+});
+$('#toggle5').click(function() {
+  $('.picture__weather5').toggle();
+  $('.text__description5').toggle();
+  $('.title__h35').toggle();
+});
+$('#toggle6').click(function() {
+  $('.picture__weather6').toggle();
+  $('.text__description6').toggle();
+  $('.title__h36').toggle();
+});
+$('#toggle7').click(function() {
+  $('.picture__weather7').toggle();
+  $('.text__description7').toggle();
+  $('.title__h37').toggle();
+});
